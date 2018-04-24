6ac6a26d618bf1ba489cb3eb5f6f982638c9e9fc
healong <37354105+healong@users.noreply.github.com>
2018-04-24 23:02:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -140,9 +140,7 @@ client.on("message", async message => {
   }
 
   if(command === "avatar") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.author.send(message.author.avatarURL);
+    const m = await message.channel.send(message.author.avatarURL);
     message.delete();
     message.channel.send(text);
   }
