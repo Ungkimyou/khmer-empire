1b94f1597b1a718036c8f003882f18da9d51eda7
healong <37354105+healong@users.noreply.github.com>
2018-04-24 18:58:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 
+const TOKEN = "NDM4MzA0MjE2ODkzNjIwMjQw.DcCs_w.wdCiCZhBfhgSIEMDMnIUsuLD_cA";
+
 var bot = Discord.Client();
 
 bot.on("message", function(message) {
     console.log(message.content);
 });
 
-bot.login(process.env.token);
+bot.login(process.env.TOKEN);
