2477824a4f7856d13a2bc4cbf46f6b123b40ed76
healong <37354105+healong@users.noreply.github.com>
2018-04-24 19:18:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const PREFIX = "k!"
 
+bot.on("message", async message => {
+    if (!message.content.startsWith(PREFIX)) return;
+
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`with TaMoToJi`);
