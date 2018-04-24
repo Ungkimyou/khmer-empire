48087f294f7f836b004fc6c7fd85c0b373acc5c6
healong <37354105+healong@users.noreply.github.com>
2018-04-24 21:22:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ client.on("guildCreate", guild => {
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`With TaMoToJi`);
 });
 
 
