4a3ea1b3ae09d1d4768943e2a95cda01f59f2c1d
healong <37354105+healong@users.noreply.github.com>
2018-04-24 19:14:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const PREFIX = "k!"
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
   bot.user.setActivity(`with TaMoToJi`);
 });
 
-bot.on("message", function(message){
-    if (message.author.equals(bot.user)) return;
 
 bot.login(process.env.TOKEN);
