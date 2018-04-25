376dd1de8c511444ce9ac90092e80542db789929
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 11:20:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -73,17 +73,6 @@ client.on("message", async message => {
     if(!message.member.permissions.has('ADMINISTRATOR')) return msg.reply('you aren\'t n admin')
     cost member = msg.mention.member.first();
      if (!member return msg.msg.reply('Invalid usage .please do `-kick @user#1234`');
-  }
-    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    let reason = args.slice(1).join(' ');
-    if(!reason) reason = "No reason provided";
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
   }
   
