c0ebad94e5b4011e49faacf01656d47f6f14360d
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 00:18:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -185,8 +185,8 @@ client.on("message", async message => {
 
      let sicon = message.guild.iconURL;
      let inviteembed = new Discord.RichEmbed()
-    .setAuthor("Invite-BOT Now :", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
-    .addField("KhmerEmpire :", "  [Click Here](https://discordapp.com/oauth2/authorize?client_id=438304216893620240&permissions=16796742&scope=bot)")
+    .setAuthor("KhmerEmpire :", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
+    .addField("BOT-Invite :", "  [Click Here](https://discordapp.com/oauth2/authorize?client_id=438304216893620240&permissions=16796742&scope=bot)")
     .addField("Discord-Invite :", "  [Click Here](https://discord.gg/WMxC5rw)")
     .setColor("#437afb")
     .setThumbnail(message.author.avatarURL)
@@ -196,6 +196,19 @@ client.on("message", async message => {
 
     }
 
+  if(command === "avatar") {
+
+     let sicon = message.guild.iconURL;
+     let avatarembed = new Discord.RichEmbed()
+     .setAuthor("KhmerEmpire :", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
+     .setDescription("Avatar !")
+     .setImage(sicon)
+     .setColor("#ebb8fa")
+     .setFooter("Bot Create By : ᴛᴀᴍᴏᴛᴏᴊɪ✓ᵛᵉʳᶦᶠᶦᵉᵈ#5881")
+            
+     message.channel.send(avatarembed);
+
+  }
 
 });
 
