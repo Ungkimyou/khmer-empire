5b49d2cdf3c86cb726ee1c824dae8213dc800601
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 21:29:39 +0700

    Update app.js

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ client.on("message", async message => {
   }
 
   if(command === "serverinfo") {
-
+     
      let sicon = message.guild.iconURL;
      let serverembed = new Discord.RichEmbed()
     .setAuthor("KhmerEmpire'say !", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
@@ -151,11 +151,11 @@ client.on("message", async message => {
  
   }
 
-  if(command === "help1") {
-
+  if(command === "cmds") {
+     
      let bicon = bot.user.displayAvatarURL;
      let botembed = new Discord.RichEmbed()
-    .setAuthor("KhmerEmpire :flag_kh: Help Commands", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
+    .setAuthor("KhmerEmpire Help Commands", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
     .setDescription("[Click Here](https://tamotoji533.wixsite.com/healong) View To Website :globe_with_meridians:")
     .setImage("https://cdn.discordapp.com/attachments/438339455141543936/438682012232974367/BotCommand.JPG")
     .setFooter("Bot Create By TaMoToJi#5881", "https://cdn.discordapp.com/avatars/438160518293880832/0301fdd6dffcaf6751d33a809a32941d.png?size=2048")
