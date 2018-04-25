e8de35727961a053258c2add2122a01b6d003358
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 12:03:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -76,13 +76,8 @@ client.on("message", async message => {
       return message.reply("Please mention a valid member of this server");
     if(!member.kickable) 
       return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention or ID
-    // join(' ') takes all the various parts to make it a single string.
     let reason = args.slice(1).join(' ');
     if(!reason) reason = "No reason provided";
-    
-    // Now, time for a swift kick in the nuts!
     await member.kick(reason)
       .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
     message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
@@ -90,8 +85,7 @@ client.on("message", async message => {
   }
   
   if(command === "ban") {
-    if(!message.member.permissions.some(r=>["ADMINISTRATOR"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
+    if(!message.member.permissions.has("ADMINISTRATOR")) return msg.reply('you aren\'t n admin');
     
     let member = message.mentions.members.first();
     if(!member)
