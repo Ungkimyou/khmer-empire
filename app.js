332467cb8beda1209a8d00a03b488c7de6a38c6a
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-25 22:15:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ client.on("message", async message => {
   if(command === "say") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join("");
+    const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
