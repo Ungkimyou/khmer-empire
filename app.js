341f54071494c50b0bd0ae953f5f0f446c3d65ed
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 22:28:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,35 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 
+
+client.on("message", message => {
+  if(message.content.indexOf(config.prefix)) return;
+  if (message.author.bot) return;
+
+  if (!points[message.author.id]) points[message.author.id] = {
+    points: 0,
+    level: 0
+  };
+  let userData = points[message.author.id];
+  userData.points++;
+
+  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+  if (curLevel > userData.level) {
+    // Level up!
+    userData.level = curLevel;
+    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+  }
+
+  if (message.content.startsWith(prefix + "level")) {
+    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+  }
+  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+    if (err) console.error(err)
+  });
+
+});
+
+
 client.on("guildMemberAdd", (member) => {
   const guild = member.guild;
   if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
