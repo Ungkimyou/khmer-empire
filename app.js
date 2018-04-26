8b0650931596dac3e2ff3ca3182fb6c197a9e69b
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 00:34:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -13,15 +13,9 @@ client.commands.set('speak', require('./commands/speak.js'));
 
 
 client.on("guildMemberAdd", (member) => {
-  const guild = member.guild;
-  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
-  newUsers[guild.id].set(member.id, member.user);
-
-  if (newUsers[guild.id].size > 10) {
-    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
-    guild.channels.find("name", "chat").send("Welcome our new users!\n" + userlist);
-    newUsers[guild.id].clear();
-  }
+  let welcomechannel = member.guild.channels.find(`name`, "welcome-channel");
+    welcomechannel.send(`DING! ${member} has walked into The server!`);
+    member.send(`Welcome to The server, **${member.user.username}**!`);  
 });
 
 
