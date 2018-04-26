80c4698dfa97edd0ef1bc5a5fadd244634ca9d31
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 03:26:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -284,12 +284,12 @@ client.on("message", async message => {
   }
 }
 
-  if(command == "removerole")
+  if(command == "removerole") {
       if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
       let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
       if(!rMember) return message.reply("Couldn't find that user, yo.");
       let role = args.join(" ").slice(22);
-      if(!role) return message.reply("Specify a role!");
+      if(!role) return message.reply("ADMIN");
       let gRole = message.guild.roles.find(`name`, role);
       if(!gRole) return message.reply("Couldn't find that role.");
 
