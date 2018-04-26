771174278f6460a0fad92c9d37e19804f8259992
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 16:38:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -274,6 +274,28 @@ e
      var cleanarr = [];
  
   }
+  
+  if (command == "test") {
+
+     const modRole = message.guild.roles.find("name", "ADMIN");
+  if (!modRole)
+    return console.log("The Mods role does not exist");
+
+  if (!message.member.roles.has(modRole.id))
+    return message.reply("You can't use this command.");
+
+  if (message.mentions.members.size === 0)
+    return message.reply("Please mention a user to kick");
+
+  if (!message.guild.me.hasPermission("KICK_MEMBERS"))
+    return message.reply("");
+
+  const kickMember = message.mentions.members.first();
+
+  kickMember.kick(reason.join(" ")).then(member => {
+    message.reply(`${member.user.username} was succesfully kicked.`);
+  });
+
 
 });
 
