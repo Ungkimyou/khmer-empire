c4622a0fdaf0544040f9b03a677a12c1c50cfc81
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 16:43:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -274,7 +274,13 @@ e
      var cleanarr = [];
  
   }
-  
+
+  if (command === "asl") {
+  let age = args[0]; // Remember arrays are 0-based!.
+  let sex = args[1];
+  let location = args[2];
+  message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+}  
 
 
 });
