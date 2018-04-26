258e46366cef644900a1f3a0a6fe73643a4cef99
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 14:10:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ e
         var mutereason = message.content.substring(mutereasondelete).split(" "); // deletes the first letters until it reaches the reason
         var mutereason = mutereason.join(" "); // joins the list kickreason into one line
         if (!mutereason) return message.reply("Please indicate a reason for the mute!") // if no reason
-        mutedmember.addRole("mute") //if reason, kick
+        mutedmember.addRole(mute) //if reason, kick
             .catch(error => message.reply(`Sorry ${message.author} I couldn't mute because of : ${error}`)); //if error, display error
         message.reply(`${mutedmember.user} has been muted by ${message.author} because: ${mutereason}`); // sends a message saying he was kicked
     }
