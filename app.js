dd0eae1272d4e39703246629fc850551aca91456
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 23:51:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ client.on("message", async message => {
     if(!client.points.has(key)) {
       client.points.set(key, {
         user: message.author.id, guild: message.guild.id, points: 0, level: 1
-      });
+      })
     }
     let currentPoints = client.points.getProp(key, "points");
     client.points.setProp(key, "points", currentPoints++);
