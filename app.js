7bf229f006de7bd9550106f201570e8c5b50e10e
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 00:12:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -345,26 +345,19 @@ client.on("message", async message => {
   } 
 
   if(command === "leaderboard") {
-  // Get a filtered list (for this guild only), and convert to an array while we're at it.
-  const filtered = client.points.filterArray( p => p.guildID === message.guild.id );
-
-  // Sort it to get the top results... well... at the top. Y'know. 
-  const sorted = filtered.sort((a, b) => a.points < b.points);
-
-  // Slice it, dice it, get the top 10 of it!
-  const top10 = sorted.splice(0, 10);
-
-  // Now shake it and show it! (as a nice embed, too!)
-  const embed = new Discord.RichEmbed()
-    .setTitle("Leaderboard")
-    .setAuthor(client.user.username, client.user.avatarURL)
-    .setDescription("Our top 10 points leaders!")
-    .setColor(0x00AE86);
-  for(data of top10) {
-    embed.addField(client.users.get(data.userID).tag, `${data.points} points (level ${data.level})`);
-  }
-  return message.channel.send({embed});
-}
+   let filtered = client.points.filterArray( p => p.guildID === message.guild.id );
+   let sorted = filtered.sort((a, b) => a.points < b.points);
+   let top10 = sorted.splice(0, 10);
+   let embed = new Discord.RichEmbed()
+     .setTitle("Leaderboard")
+     .setAuthor(client.user.username, client.user.avatarURL)
+     .setDescription("Our top 10 points leaders!")
+     .setColor(0x00AE86);
+   for(data of top10) {
+     embed.addField(client.users.get(data.userID).tag, `${data.points} points (level ${data.level})`);
+   }
+   return message.channel.send({embed});
+ }
 
   if(command === "eval") {
       const code = args.join(" ");
