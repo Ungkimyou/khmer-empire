954ec82649dadabb110bf497b91f87b67f6725ef
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 18:35:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -36,12 +36,6 @@ client.on("message", async message => {
   if(message.content.indexOf(config.prefix) !== 0) return;
   if(message.author.id !== config.ownerID) return;
 
-  if(message.content.startsWith(config.prefix + "prefix")) {
-  // Gets the prefix from the command (eg. "!prefix +" it will take the "+" from it)
-  let newPrefix = message.content.split(" ").slice(1, 2)[0];
-  // change the configuration in memory
-  config.prefix = newPrefix;
-
   // Now we have to save the file.
   fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error);
  
