c0673aca02981424495fe87da4c85dabc22c8c3c
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 15:10:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -267,7 +267,7 @@ e
 
   if(command === "purge") {
         let messagecount = parseInt(args[1]) || 1;
-        let deletedMessages = -1;
+        var deletedMessages = -1;
         message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
             messages.forEach(m => {
                 if (m.author.id == bot.user.id) {
@@ -281,6 +281,12 @@ e
                     .then(m => m.delete(2000));
         }).catch(console.error);
     }
+
+  if (command == "cookie") { // creates the command cookie
+        if (args[1]) message.channel.send(message.author.toString() + " has given " + args[1].toString() + " a cookie! :cookie:") // sends the message saying someone has given someone else a cookie if someone mentions someone else
+        else message.channel.send("Who do you want to send a cookie to? :cookie: (Correct usage: *cookie @username)") // sends the error message if no-one is mentioned
+    }
+
 
 });
 
