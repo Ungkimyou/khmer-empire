4eeb5a21f35ede93c2b73e466c5e7e5f5a2cf27d
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 17:37:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -310,13 +310,7 @@ e
 
  }
   
-  if(command === "row") {
-    if (!row) { // Can't find the row.
-     sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
-     } else { // Can find the row.
-     sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
-  
- }
+
 
 });
 
