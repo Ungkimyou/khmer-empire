0bc00c3aafff153a5ebfb17494e2f215cb3cee20
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 22:14:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -36,29 +36,19 @@ client.on("guildCreate", guild => {
 client.on("presenceUpdate", (oldMember, newMember) => {
   let guild = newMember.guild;
   let playRole = guild.roles.find("name", "Playing PLAYERUNKNOWN'S BATTLEGROUNDS");
-  if(!playRole) return;
-  
-  if(newMember.user.presence.game && newMember.user.presence.game.name === "PLAYERUNKNOWN'S BATTLEGROUNDS") {
-    newMember.addRole(playRole);
-  } else if(!newMember.user.game && newMember.roles.has(playRole.id)) {
-    newMember.removeRole(playRole);
-  let guild = newMember.guild;
   let playRole = guild.roles.find("name", "Playing Minecraft");
-  if(!playRole) return;
-  
-  if(newMember.user.presence.game && newMember.user.presence.game.name === "Minecraft") {
-    newMember.addRole(playRole);
-  } else if(!newMember.user.game && newMember.roles.has(playRole.id)) {
-    newMember.removeRole(playRole);
-  let guild = newMember.guild;
   let playRole = guild.roles.find("name", "Playing Counter-Strike: Global Offensive");
-  if(!playRole) return;
-  
+  if(!playRole) return;  
+  if(newMember.user.presence.game && newMember.user.presence.game.name === "PLAYERUNKNOWN'S BATTLEGROUNDS") {
+  if(newMember.user.presence.game && newMember.user.presence.game.name === "Minecraft") {
   if(newMember.user.presence.game && newMember.user.presence.game.name === "Counter-Strike: Global Offensive") {
     newMember.addRole(playRole);
   } else if(!newMember.user.game && newMember.roles.has(playRole.id)) {
     newMember.removeRole(playRole);
-  }
+  let guild = newMember.guild;
+
+  if(!playRole) return;
+  
 });
 
 
