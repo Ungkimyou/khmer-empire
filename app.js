3734ecade02ee9ec11adf49c07891a7302dcdb13
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 01:55:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,6 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 
-const swearWords = ["fuck", "shit", "sh*t", "dick"];
-if( swearWords.some(word => message.content.toLowerCase().includes(word)) {
- message.reply("Oh no you said a bad word!!!");
-}
-
 
 client.commands = new Discord.Collection();
 
