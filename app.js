259b2b46551d2c3a13fcd8ed3c155ee486a7823a
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-26 14:28:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ client.on("message", async message => {
   // Also good practice to ignore any message that does not start with our prefix, 
   // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
+
+  var mutedrole = message.guild.roles.find("name", "muted");
   
   // Here we separate our "command" name, and our "arguments" for the command. 
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
