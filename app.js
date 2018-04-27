dd522a585e6d4c5722bd4b6886a1f531e2da90bb
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 14:38:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -109,33 +109,6 @@ client.on("message", async message => {
   const command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
-  if(command === "xp") {
-      if(!xp[message.author.id]){
-        xp[message.author.id] = {
-        xp: 0,
-        level: 1
-   };
- }
-  let curxp = xp[message.author.id].xp;
-  let curlvl = xp[message.author.id].level;
-  let nxtLvlXp = curlvl * 300;
-  let difference = nxtLvlXp - curxp;
-
-  let lvlEmbed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor(purple)
-  .addField("Level", curlvl, true)
-  .addField("XP", curxp, true)
-  .setFooter(`${difference} XP til level up`, message.author.displayAvatarURL);
-
-  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
-
-}
-
-
-
-
-
   if(command === "ping") {
 
     const m = await message.channel.send(":mag: Starting ...?");
