f50e738c31c79e6c21be1cfbd14fa788babe2f9c
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 13:16:41 +0700

    Update app.js

--- app.js
+++ app.js
@@ -93,32 +93,24 @@ client.on("presenceUpdate", (oldMember, newMember) => {
 });
 
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
+
   if(message.author.bot) return;
   
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
+
   if(message.content.indexOf(config.prefix) !== 0) return;
 
-  // Now we have to save the file.
+
 
   var mutedrole = message.guild.roles.find("name", "Muted");
   
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
   
   if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+
     const m = await message.channel.send(":mag: Starting ...?");
     m.edit(`Your Ping is ${m.createdTimestamp - message.createdTimestamp}ms :satellite: `);
   }
@@ -126,7 +118,6 @@ client.on("message", async message => {
 
 
   if(command === "say") {
-    if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply("you don't have permisson [MANAGE_MESSAGE] to do this !);
     const sayMessage = args.join(" ");
     message.delete().catch(O_o=>{}); 
     message.channel.send(sayMessage);
@@ -134,7 +125,6 @@ client.on("message", async message => {
 
 
   if(command == "embed") {
-    if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.reply("you don't have permisson [MANAGE_MESSAGE] to do this !);
     const embed1 = new Discord.RichEmbed()
     .setTitle(args.join(" "))
     .setDescription(args.join(" "))
