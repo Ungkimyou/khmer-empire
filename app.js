774fa7557325b918f99c8af0526c2716fe17e9e0
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 15:30:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+const fs = require("fs")
 
 const talkedRecently = new Set();
 
