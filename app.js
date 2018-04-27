ea89494f0cb8b18ab25ac8e5efe854793b3922a8
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-27 13:01:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -374,7 +374,7 @@ client.on("message", async message => {
 
   if(command === "clean") {
     if(!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.sand("you don't have permisson to do this !");
-    if(!args[0]) return message.channel.send("no");
+    const deleteCount = parseInt(args[0], 1);
     message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
  });
