9b1d91f1c623619cd50469cc15b10283c5803dc7
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-29 01:01:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -387,6 +387,18 @@ client.on("message", async message => {
  
   }
 
+  if(command === 'botinfo') {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.RichEmbed()
+    .setDescription("Bot Information")
+    .setColor("#15f153")
+    .setThumbnail(bicon)
+    .addField("Bot Name", bot.user.username)
+    .addField("Created On", bot.user.createdAt);
+
+    return message.channel.send(botembed);
+}
+
 
 
   if(command === "rsetup") {
