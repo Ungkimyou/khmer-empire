4045daee1e96241418d02f78df7656dea154ae5a
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-28 22:27:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -403,11 +403,10 @@ client.on("message", async message => {
 
   if(command === "avatar") {
 
-     let sicon = message.mentions.iconURL;
      let avatarembed = new Discord.RichEmbed()
      .setAuthor("KhmerEmpire :", "https://cdn.discordapp.com/avatars/438304216893620240/35ccf504013fd1b7870a3d717ede2ec4.jpg?size=2048")
      .setDescription("Avatar !")
-     .setImage(message.user.avatarURL)
+     .setImage(message.mentions.avatarURL)
      .setColor('RANDOM')
      .setFooter("Bot Create By : ᴛᴀᴍᴏᴛᴏᴊɪ✓ᵛᵉʳᶦᶠᶦᵉᵈ#5881")
             
