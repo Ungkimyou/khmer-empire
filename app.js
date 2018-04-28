0c9020643be2790013dad15ae1e22ff3a646df9b
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-29 00:04:01 +0700

    Update app.js

--- app.js
+++ app.js
@@ -224,6 +224,7 @@ client.on("message", async message => {
 			.catch(collected => {
 				console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
 				message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
+                                message.delete()
 			});
 	}
 
