2ba24590ef2ba2e6a253d6bee056d4796173affe
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-28 19:34:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -528,7 +528,7 @@ client.on("message", async message => {
   if(command === "addrole") {
    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
     let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-    if(!rMember) return message.reply("Couldn't find that user, yo.");
+    if(!rMember) return message.reply("```md Please mention user you want to addrole \n- k!addrole [@user] [Roles]``");
     let role = args.join(" ").slice(22);
     if(!role) return message.reply("Specify a role!");
     let gRole = message.guild.roles.find(`name`, role);
