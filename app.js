0efed89d095bf93a4fb28caf8efe30dcb7429a75
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-28 21:21:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -462,23 +462,34 @@ client.on("message", async message => {
     if (command == "help") { // creates a command *help
         let embedhelpmember = new Discord.RichEmbed() // sets a embed box to the variable embedhelpmember
             .setTitle("**List of Commands**\n") // sets the title to List of Commands
-            .addField(" - help", "Displays this message (Correct usage: *help)") // sets the first field to explain the command *help
+            .addField(" - report". "report player from server k!report [user] [reason]")
+            .addField(" - help", "Displays this message (Correct usage: k!help)") // sets the first field to explain the command *help
             .addField(" - info", "Tells info about myself :grin:") // sets the field information about the command *info
-            .addField(" - ping", "Tests your ping (Correct usage: *ping)") // sets the second field to explain the command *ping
-            .addField(" - cookie", "Sends a cookie to the desired player! :cookie: (Correct usage: *cookie @username)") // sets the third field to explain the command *cookie
-            .addField(" - 8ball", "Answers to all of your questions! (Correct usage: *8ball [question])") // sets the field to the 8ball command
-            .setColor(0xFFA500) // sets the color of the embed box to orange
-            .setFooter("You need help, do you?") // sets the footer to "You need help, do you?"
+            .addField(" - ping", "Tests your ping (Correct usage: k!ping)") // sets the second field to explain the command *ping
+            .addField(" - cookie", "Sends a cookie to the desired player! :cookie: (Correct usage: k!cookie @username)") // sets the third field to explain the command *cookie
+            .addField(" - gay", "bot is tell your gay or not") 
+            .addField(" - dick", "bot is tell your dick size")
+            .addField(" - Invite", "Invite KhmerEmpire to the server !")
+            .addField(" - serverinfo ", "tell info about server ")
+            .addField(" - helpadmin ", "list of admin commands - allow user have permsisson ADMINISTRATOR")
+            .setColor('RANDOM') // sets the color of the embed box to orange
+            .setFooter("Support Server : https://discord/7mS9GEY") // sets the footer to "You need help, do you?"
+        message.channel.send(embedhelpmember);
+
+    if (command == "helpadmin") {
         let embedhelpadmin = new Discord.RichEmbed() // sets a embed box to the var embedhelpadmin
             .setTitle("**List of Admin Commands**\n") // sets the title
-            .addField(" - say", "Makes the bot say whatever you want (Correct usage: *say [message])")
-            .addField(" - mute", "Mutes a desired member with a reason (Coorect usage: *mute @username [reason])") // sets a field
-            .addField(" - unmute", "Unmutes a muted player (Correct usage: *unmute @username)")
-            .addField(" - kick", "Kicks a desired member with a reason (Correct usage: *kick @username [reason])") //sets a field
-            .setColor(0xFF0000) // sets a color
-            .setFooter("Ooo, an admin!") // sets the footer
-        message.channel.send(embedhelpmember); // sends the embed box "embedhelpmember" to the chatif
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.reply("Sorry : you don't have MUTE_MEMBERS permission to do this ");
+            .addField(" - say", "Makes the bot say whatever you want (Correct usage: k!say [message])")
+            .addField(" - mute", "Mutes a desired member with a reason (Coorect usage: k!mute @username [reason])") // sets a field
+            .addField(" - unmute", "Unmutes a muted player (Correct usage: k!unmute @username)")
+            .addField(" - kick", "Kicks a desired member with a reason (Correct usage: k!kick @username [reason])") //sets a field
+            .addField(" - ban" , "ban a player u want to ban k!ban @username [reason]")
+            .setColor('RANDOM') // sets a color
+            .addField(" - clear" , "clear message from channel k!clear [am] ") 
+            .addField(" - rsetup", "rsetup is report setup channel ")
+            .setFooter("Support Server : https://discord/7mS9GEY") // sets the footer
+        message.channel.send(embedhelpadmin); // sends the embed box "embedhelpmember" to the chatif
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry : you don't have ADMINISTRATOR permission to do this ");
     }
 
 
