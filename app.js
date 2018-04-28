497c8fc1e6425f2132395525ed55f615d81a70ce
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-28 18:22:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -136,6 +136,20 @@ client.on("presenceUpdate", (oldMember, newMember) => {
   }  
 });
 
+client.on("presenceUpdate", (oldMember, newMember) => {
+  let guild = newMember.guild;
+  let playRole = guild.roles.find("name", "Playing Grand Theft Auto V");
+  if(!playRole) return;  
+
+  if(newMember.user.presence.game && newMember.user.presence.game.name === "Grand Theft Auto V") {
+    newMember.addRole(playRole);
+  } else if(!newMember.user.game && newMember.roles.has(playRole.id)) {
+    newMember.removeRole(playRole);
+  let guild = newMember.guild;
+  if(!playRole) return;
+    
+  }  
+});
 
 
 client.on("message", async message => {
