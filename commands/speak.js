cd32c04094095b611b5b8d4c560d2495a25ae7bf
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-29 12:27:03 +0700

    Delete speak.js

--- commands/speak.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, message, args) => {
-  if (message.author.id !== config.ownerID) return message.reply('Arrooo???');
-  message.channel.send(args.join(' '));
-  message.delete();
-};
