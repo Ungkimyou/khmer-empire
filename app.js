e46af2c21d176ae2333927e4a05876ad2b9a1abd
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-30 23:11:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -141,18 +141,16 @@ if( swearWords.some(word => message.content.includes(word)) ) {
   if(!tomute) return message.reply("Couldn't find user.");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
   let muterole = message.guild.roles.find(`name`, "TempMuted");
-  //end of create role
   let mutetime = args[1];
   if(!mutetime) return message.reply("You didn't specify a time!");
-
   await(tomute.addRole(muterole.id));
   message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
-
   setTimeout(function(){
     tomute.removeRole(muterole.id);
     message.channel.send(`<@${tomute.id}> has been unmuted!`);
-  }, ms(mutetime))
+ }, ms(mutetime));
 }
+
 
   if(command === "ping") {
     const newemb = new Discord.RichEmbed()
