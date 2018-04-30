d43e641875fdf6c33a6b142b646c6f34c7133a54
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-30 22:23:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const weather = require('weather-js');
 
 const swearWords = ["fuck", "shit", "frak", "shite"];
 
@@ -134,12 +135,35 @@ if( swearWords.some(word => message.content.includes(word)) ) {
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
-
+ if(command === "weather") {
+    weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
+     if (err) message.channel.send(err);
+     if (result === undefined || result.length === 0) {
+         message.channel.send('**Please enter a location!**')
+         return;
+     }
+     var current = result[0].current;
+     var location = result[0].location;
+     const embed = new Discord.RichEmbed()
+          .setDescription(`**${current.skytext}**`)
+          .setAuthor(`Weather for ${current.observationpoint}`)
+          .setThumbnail(current.imageUrl)
+          .setColor(0x00AE86)
+          .addField('Timezone',`UTC${location.timezone}`, true)
+          .addField('Degree Type',location.degreetype, true)
+          .addField('Temperature',`${current.temperature} Degrees`, true)
+          .addField('Feels Like', `${current.feelslike} Degrees`, true)
+          .addField('Winds',current.winddisplay, true)
+          .addField('Humidity', `${current.humidity}%`, true)
+          message.channel.send({embed});
+  })
+}
 
   if(command === "ping") {
     const newemb = new Discord.RichEmbed()
+    .setTitle(message.author.tag, true)
     .setColor(0xFFBF00)
-    .addField('```Ping : ```', new Date().getTime() - message.createdTimestamp + " ms ")
+    .addField('```Ping : ```', new Date().getTime() - message.createdTimestamp + " ms ", true)
     message.channel.send({embed: newemb})
 }
 
