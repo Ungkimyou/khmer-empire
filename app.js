dd7be4ca4de941b2ff634eb7e5f1d13667f5d175
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-30 15:08:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,23 @@ const responses = [
    'Yes , You Gay !', 'No , You Not Gay !', 'Maybe', 'I Don`t Know', 'Nope', 'Gay . 50%'
 ]
 
+function setActivity() {
+    //Variable Array for what the setGame can be set to
+    var Gameinfo = [`Prefix: ${config.prefix}`, `Running on ${client.guilds.size} Servers`, `Running Commands`, `Try ${config.prefix}help`, `${config.prefix}help`,
+        `Using ${(((process.memoryUsage().heapUsed)/1024)/1024).toFixed(0)}Mb's of RAM`, `Ping to API: ${(client.ping).toFixed(0)} Ms`, `BORK!!!` // Change these to what you want, add as many or as few as you want to
+    ]
+
+    var info = Gameinfo[Math.floor(Math.random() * Gameinfo.length)]; //Random Math to set the setGame to something in the GameInfo array
+
+    client.user.setActivity(info) // "playing Game" '...' Sets the setGame to what the info Random math picked from the GameInfo Array
+    if (config.debugMode === "1") {
+        console.log(`[ LOG ] set Activity set to ( ${info} )`) //Logs to console what the setGame was set as.
+    }
+
+}
+
+setInterval(setActivity, 1000 * 60 * 2)
+
 
 const talkedRecently = new Set();
 
