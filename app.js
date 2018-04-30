11eebb673596740d5f503b0f031f6ca1392b10c2
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-04-30 22:09:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const superagent = require("superagent");
 
 const swearWords = ["fuck", "shit", "frak", "shite"];
 
