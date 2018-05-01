4885ad640b87331985b60b99ad0f646d73bd3da0
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-01 15:57:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const talkedRecently = new Set();
 
 client.commands = new Discord.Collection();
 
-
+const sm = require('string-similarity');
 
 client.on("message", (message) => {
   if(message.content === "k!") {
@@ -269,6 +269,35 @@ if( swearWords.some(word => message.content.includes(word)) ) {
   message.channel.send({embed: reverseEmbed})
     
 }
+
+  if(command === "avt") {
+  let members = [];
+  let indexes = [];
+  
+  message.guild.members.forEach(function(member){
+    members.push(member.user.username);
+    indexes.push(member.id);
+  })
+  let match = sm.findBestMatch(args.join(' '), members);
+  let username = match.bestMatch.target;
+  let member = message.guild.members.get(indexes[members.indexOf(username)])
+  let usernameUser = '';
+  let UserMention = '';
+  
+  if(!args[0]) {
+    usernameUser = message.author
+    UserMention = message.member
+  } else {
+  let mention = message.mentions.users.first()
+    usernameUser = mention || member.user
+    UserMention = message.mentions.members.first() || message.guild.members.get(args[0]) || member
+  }
+  message.channel.send({embed: new Discord.MessageEmbed()
+                        .setAuthor(`${usernameUser.tag}'s avatar`)
+                        .setColor('RANDOM')
+                        .setImage(usernameUser.displayAvatarURL({size: 2048}))
+  })
+};
 
 
   if(command === "gay") {
