b3c744f3b22be39060d72f7bfdfe1726cc3e11bc
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-01 16:30:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
-const shorten = require("isgd");
-
 
 const swearWords = ["fuck", "shit", "frak", "shite"];
 
@@ -206,35 +204,6 @@ if( swearWords.some(word => message.content.includes(word)) ) {
   })
  }
   
-  if(command === "ls") {
-  if (!args[0]) return message.channel.send('**Proper Usage: !shorten <URL> [title]**')
- 
-  // First, we need to check if they entered an optional title
-  if (!args[1]) { // If the second argument in the message is undefined, run this
-   
-    shorten.shorten(args[0], function(res) { // This will run the shorten function and pass it 'res'
-      if (res.startsWith('Error:')) return message.channel.send('**Please enter a valid URL**'); // The only possible error here would be that it's not a valid URL.
-     
-      message.channel.send(`**<${res}>**`); // If no error encountered, it will return with the response in the res variable
-   
-    })
-   
-  } else { // If the second argument IS defined, run this
-   
-    shorten.custom(args[0], args[1], function(res) { // This is sort of the same thing from earlier, although this will pass the first and second arguments to the shortener, then return 'res'
-     
-      // There are a few possible error messages, so we can just tell them what the shortener says
-      if (res.startsWith('Error:')) return message.channel.send(`**${res}**`); // This will return the full error message
-      // Make sure you return, so it doesn't run the rest of the code
-     
-      message.channel.send(`**<${res}>**`); // If no errors encountered, it will return the link.
-     
-     
-    }) // We also can use <> to make sure it doesn't show an embed, now let's test it!
-   
-  }
- 
-}
 
   if(command === "pat") {
      var images = ["https://cdn.discordapp.com/attachments/424667806320033814/437807617965031424/unnamed_1.gif", "https://cdn.glitch.com/5df641e3-8d98-4abb-9045-d5482434003a%2FJake_pat.gif?1524497996034", "https://media.tenor.com/images/cdc004bbbaba6f60d8e62a1f127516e0/tenor.gif"];
