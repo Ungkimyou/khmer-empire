a6df4e7a5650befac0621ddff1124a65db30abf4
TaMoToJi <37354105+TaMoToJi@users.noreply.github.com>
2018-05-07 18:15:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1251,7 +1251,6 @@ if (command === "myinfo") {
     .setFooter(`Requested by ${message.author.tag}`);
     message.channel.send(avatarEmbed)
   }
-}
 
   if(command === "username") {
      let sicon = message.guild.iconURL;
@@ -1263,21 +1262,6 @@ if (command === "myinfo") {
      message.channel.send(usernameembed);
   } 
   
-  if(command === "eval") {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
-
-      message.channel.send(clean(evaled), {code:"xl"});
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-      message.delete().catch(O_o=>{}); 
-      message.channel.send(sayMessage);
-      message.delete()
-    
-  }
-
     if (command == "mute") { // creates the command mute
     if(!message.member.permissions.has("MUTE_MEMBERS")) return message.channel.send("you don't have MUTE_MEMBERS permissions to use this !"); // if author has no perms
         var mutedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
@@ -1376,12 +1360,6 @@ if (command === "myinfo") {
 
 });
 
-function clean(text) {
-  if (typeof(text) === "string")
-    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-  else
-      return text;
-}
 
 client.login(config.token);
            
